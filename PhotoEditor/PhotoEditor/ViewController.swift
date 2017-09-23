299c07c8c067cca8d047d024ad333f4eb290b525
iluvdadong <iluvdadong@naver.com>
2017-09-24 03:31:53 +0900

    take photo action IS ADDED

--- PhotoEditor/PhotoEditor/ViewController.swift
+++ PhotoEditor/PhotoEditor/ViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+    @IBOutlet weak var imgView: UIImageView!
 
+    @IBAction func actionClickOnCamera(sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+        
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.delegate = self
+            
+            present(imagePicker, animated: true,completion: nil)
+        } else {
+        
+            let alert = UIAlertController(title: "Alert", message: "You don't have a camera for this device", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    
+    }
+    
+    @IBAction func actionClickOnGallery(sender: AnyObject) {
+    
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
